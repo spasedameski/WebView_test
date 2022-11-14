28d3c9f767c9bf3d114eaeea6c298489c3e60fce
s_dameski <spasedameski@gmail.com>
2022-11-14 14:04:38 +0100

    Initial commit

---
+++ app/src/main/java/com/kaidotech/webview_test/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.kaidotech.webview_test
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+        // this will load the url of the website
+        webView.loadUrl("https://kaidotech.de/")
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+}
